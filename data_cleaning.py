808e0896227ba3bc8610a43bd035adeca75bc9d9
Wassim brs <wassim.bouras@outlook.com>
2026-01-07 14:09:35 +0100

    ajout fonction filtrage duplicats

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 
-data = pd.read_csv("sample_data.csv")
+data = pd.read_csv("flickr_data2.csv")
+data['date_key'] = data['date_taken_year'].astype(str) + '-' + \
+                    data['date_taken_month'].astype(str) + '-' + \
+                    data['date_taken_day'].astype(str)
 
 # pour un point donné (lat, lon), retourne True si il est dans Lyon
 def is_within_lyon(lat: float, lon: float) -> bool:
@@ -16,8 +19,40 @@ def is_within_lyon(lat: float, lon: float) -> bool:
 
 # filtre toutes les lignes du DataFrame pour ne garder que celles situées à Lyon
 def filter_lyon(data: pd.DataFrame) -> pd.DataFrame:
-    mask = data[" lat"].between(45.6, 45.9) & data[" long"].between(4.7, 5.0)
+    mask = data["lat"].between(45.6, 45.9) & data["long"].between(4.7, 5.0)
     return data.loc[mask].copy()
+
+def filter_same_picture(df: pd.DataFrame) -> pd.DataFrame:
+    print(f"Avant nettoyage. Lignes restantes : {len(df)}")
+
+    # On crée une colonne 'jour' pour regrouper les photos du même jour
+    df['date_key'] = df['date_taken_year'].astype(str) + '-' + \
+                    df['date_taken_month'].astype(str) + '-' + \
+                    df['date_taken_day'].astype(str)
+
+    # On définit ton "epsilon" (ex: 0.0001 degré correspond à environ 11m)
+    epsilon = 0.00001 
+
+    # On trie pour que les photos suspectes d'être des doublons soient côte à côte
+    df = df.sort_values(by=['user', 'date_key', 'lat', 'long'])
+
+    # On calcule la différence avec la ligne du dessus
+    df['diff_lat'] = df['lat'].diff().abs()
+    df['diff_lon'] = df['long'].diff().abs()
+    df['same_user'] = df['user'] == df['user'].shift()
+    df['same_day'] = df['date_key'] == df['date_key'].shift()
+
+    # On identifie les doublons : même user, même jour ET différence < epsilon
+    is_duplicate = (df['same_user']) & \
+                (df['same_day']) & \
+                (df['diff_lat'] < epsilon) & \
+                (df['diff_lon'] < epsilon)
+
+    # On garde les données propres
+    df_cleaned = df[~is_duplicate].drop(columns=['diff_lat', 'diff_lon', 'same_user', 'same_day'])
+
+    print(f"Nettoyage terminé. Lignes restantes : {len(df_cleaned)}")
+    return df_cleaned
 
 
 if __name__ == "__main__":
@@ -26,6 +61,10 @@ if __name__ == "__main__":
     data_lyon = filter_lyon(data)
     after = len(data_lyon)
 
+    cleaned = filter_same_picture(data)
+
     print(f"Lignes avant filtre Lyon : {before}")
     print(f"Lignes apres filtre Lyon : {after}")
-    print(data_lyon.head())
+    print(data_lyon.sort_values(by=['user', 'date_key', 'lat', 'long']).head())
+    print(cleaned.head())
+
